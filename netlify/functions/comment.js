af3f79c5b511b903fb7c55100dac6d9029cc36f5
Jovial <142502164+JovialisS@users.noreply.github.com>
2026-01-10 18:25:12 +0800

    Add files via upload

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -4,6 +4,9 @@ const serverless = require('serverless-http');
 
 const app = Waline({
   env: 'netlify',
+
+  siteName: '星海拾忆 Blog', 
+  
   async postSave(comment) {
     // do what ever you want after save comment
   },
